107c88378107f51374814c8ffc1fa14391cb286f
ricardo-shinoda <ricardoshinoda@gmail.com>
2023-10-10 21:54:50 -0300

    python variables and quotes

---
+++ quotes.py
@@ -0,0 +1,11 @@
+string = "devops"
+
+print("string with double")
+print('string with a single')
+print(string)
+
+print('''
+This is a paragraph string
+This is the second line
+This is the last one
+''')
