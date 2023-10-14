d80d9be633c1e8b60b961a500f4957b6dd394fa7
ricardo-shinoda <ricardoshinoda@gmail.com>
2023-10-13 22:40:10 -0300

    fix project structure

---
+++ src/variable.py
@@ -0,0 +1,12 @@
+# multiple assigments
+
+a = b = c = 65
+print(a)
+
+# Multiple assignments with multiple values
+x, y, z = "zee", "why", 12
+print("The values are:", x, y, z)
+
+
+# Add type to see what tye the item is
+print("Variable x is", type(z))
---
+++ src/xlx_converter.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import json
+
+# Carregue o arquivo .xlsx em um DataFrame do pandas
+df = pd.read_excel('invoice.xlsx')
+
+# Converta a coluna "Data de Compra" para o formato de data (se necessário)
+df['Data de Compra'] = pd.to_datetime(df['Data de Compra'], errors='coerce')
+
+# Substitua 'Data de Compra' pelo nome correto da coluna de datas no seu arquivo
+df['Data de Compra'] = df['Data de Compra'].dt.strftime('%Y-%m-%d')
+
+# Criar uma função para serializar as datas
+
+
+def date_converter(o):
+    if isinstance(o, pd.Timestamp):
+        return o.strftime('%Y-%m-%d')
+    return str(o)
+
+
+# Converta o DataFrame para uma lista de dicionários
+data = df.to_dict(orient='records')
+
+# Escreva o dicionário em um arquivo JSON com a função de conversão de data
+with open('saida.json', 'w', encoding='utf-8') as json_file:
+    json.dump(data, json_file, indent=4,
+              default=date_converter, ensure_ascii=False)
+
+print("Conversão concluída. Os dados foram salvos em 'saida.json'.")
+
+soma_valores = sum(item['Valor (em R$)'] for item in data)
+print(f'Soma dos valores: {soma_valores}')
