01af174b8cb8443d9b234c987feeffffc93003e6
ricardo-shinoda <ricardoshinoda@gmail.com>
2023-11-28 21:48:38 -0300

    updating files

---
+++ src/loop.py
@@ -0,0 +1,14 @@
+# For loop
+for i in range(10):
+    print("Cannot connect to the destination")
+
+
+list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for i in list:
+    print(i)
+
+fruits = ["apple", "pinaple", "banana", "avocado"]
+for i, fruits in enumerate(fruits):
+    print(f'Index {i}: {fruits}')
+
+# While loop
