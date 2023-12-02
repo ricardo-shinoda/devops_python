d8898c80740aa08a40d54c3a0fc01efc621add4b
ricardo-shinoda <ricardoshinoda@gmail.com>
2023-12-02 19:33:53 -0300

    updating files

--- src/open_file.py
+++ src/open_file.py
@@ -1,23 +1,22 @@
 # How to open a file with python
 # The function is: open()
+# parsing: is a process of converting data into a more readable format
+# .split convert a string into a list
 
 with open("/home/ricardo/codes/devops_python/src/login_attempts.txt", "r") as file:
     file_text = file.read()
 usernames = file_text.split()
 
 
-# parsing: is a process of converting data into a more readable format
-
-# .split convert a string into a list
 def login_check(login_list, current_user):
     counter = 0
     for i in login_list:
         if i == current_user:
             counter = counter + 1
     if counter >= 3:
-        print("You have been blocked for more than 3 tentatives")
+        return "You have been blocked for more than 3 tentatives"
     else:
-        print("You can login:", i)
+        return "You can login"
 
 
-login_check(usernames, "luisa")
+print(login_check(usernames, "lucas"))
